9fd6a145fde2593466149e9a54b773a3e03fe19e
ayoub bani <ayoub.b@dsi-cs.com>
2021-01-25 11:51:42 +0100

    init

---
+++ src/components/todos.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <div class="bg-white">
+      <!-- Card -->
+      <div class="card border-0 mt-3 bg-primary d-flex justify-content-center">
+        <div class="container hover d-flex align-items-center">
+          <span class="font-weight-bold mr-auto">{{name}}</span>
+          <a @click="$emit(deleteTodos(index))" class="bg-white rounded p-1" href="#"><img class="custom-image-size" src="@/assets/images/wrong.png" alt="delete this todo icon"></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "todos",
+    components: {},
+    props: {
+      name: {
+        type: String,
+        required: true
+      }
+    },
+    data() {
+      return {
+        list: []
+      }
+    },
+
+    methods: {
+      add() {
+
+      },
+      complete() {
+
+      },
+      deleteTodos() {
+      }
+    }
+  };
+</script>
+
+<style>
+
+</style>
